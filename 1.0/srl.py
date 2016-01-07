6c747765bd1d1c790a1fd974727c48ae9a48c011
memoryleak47 <memoryleak47@protonmail.ch>
2016-01-07 22:10:08 +0100

    1.0: added SRLParser; @import now works

--- 1.0/srl.py
+++ 1.0/srl.py
@@ -2,20 +2,63 @@
 
 import os
 import sys
+import re
 
 usage="srl.py <file>"
 
-def uncommentSRL(lines):
-	result=""
-	for line in lines:
-		if line.find("#") != -1: # if a comment exists
-			line = line[:line.find("#")] # clear it
-		line = line.strip() # removes whitespaces tabs and endlines from beginning and end of line
-		result += line
-	return result
+class SRLParser:
+	@staticmethod
+	def __uncomment(code): # uncomments; strips
+		result=""
+		for line in code:
+			if line.find("#") != -1: # if a comment exists
+				line = line[:line.find("#")] # clear it
+			line = line.strip() # removes whitespaces tabs and endlines from beginning and end of line
+			result += line
+		return result
 
-def parseSRL(lines):
-	print(uncommentSRL(lines))
+	@staticmethod
+	def __parseImportKeyword(code, rules):
+		filename = re.match(r'^@import\("([^"]|\\")+"\)\.', code).group()[9:-3]
+		filepath = os.path.dirname(sys.argv[1]) + "/" + filename
+		try:
+			importfile=open(filepath)
+		except:
+			print("Can't import file " + filepath)
+			sys.exit()
+		importrules = SRLParser.parse(importfile.readlines())
+		importfile.close()
+
+		for rule in importrules:
+			rules.append(rule)
+
+		return code[len(filename)+12:]
+
+	@staticmethod
+	def __parsePrintKeyword(code, rules):
+		return code
+
+	@staticmethod
+	def __parseKeyword(code, rules):
+		if re.match(r'^@import\("([^"]|\\")+"\)\.', code) != None:
+			return SRLParser.__parseImportKeyword(code, rules)
+		elif re.match(r'^@print\(.*\)\.', code) != None: # TODO
+			return SRLParser.__parsePrintKeyword(code, rules)
+		else:
+			print("invalid keyword \"" + code + "\"") # make better
+			sys.exit()
+
+	@staticmethod
+	def parse(code): # returns list() of rules
+		rules=list()
+		code = SRLParser.__uncomment(code)
+		while code != "": # as long as code has to be converted to a rule
+			if code.startswith("@"):
+				code = SRLParser.__parseKeyword(code, rules)
+			else:
+				print("Can't parse code \"" + code + "\"")
+				sys.exit()
+		return rules
 
 if len(sys.argv) != 2:
 	print(usage)
@@ -26,5 +69,6 @@ else:
 	except:
 		print("file " + sys.argv[1] + " not found")
 		sys.exit()
-	parseSRL(openfile.readlines())
+	rules=SRLParser.parse(openfile.readlines())
+	print(rules) # remove
 	openfile.close()
