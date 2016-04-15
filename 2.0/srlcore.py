5925fd1b780e28f59a3b545ae1a8dfa24ef85d92
memoryleak47 <memoryleak47@protonmail.ch>
2016-04-15 09:35:35 +0200

    worked srlcore.py

--- 2.0/srlcore.py
+++ 2.0/srlcore.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3.4
 
 import sys
+import os
 
 def getSubSigns():
 	return ["->"]
+
+def containsSubSigns(string):
+	for sign in getSubSigns():
+		if sign in string:
+			return True
+	return False
+
+def splitAtSubSigns(string):
+	for sign in getSubSigns():
+		if sign in string:
+			return string.split(sign)
+	die("splitAtSubSigns: NOPE NOPE NOPE")
+	return None
 
 def getCellEndSigns():
 	return [",", ".", ")"]
@@ -31,10 +45,59 @@ class SRLSystem:
 		print()
 
 	def loadFromFile(self, filename):
-		print("loadFromFile() TODO")
+		rules = self.__getRulesByFile(filename, sys.path[0])
+		for rule in rules:
+			if isinstance(rule, SubRule):
+				self.subrules.append(rule)
+			elif isinstance(rule, RelRule):
+				self.relrules.append(rule)
+			else:
+				die("loadFromFile: NOPE NOPE NOPE")
 
 	def __getRulesByFile(self, filename, pwd):
-		print()	
+		abspath = os.path.realpath(pwd + "/" + filename)
+		if abspath in self.importedfiles:
+			return
+		rules=list()
+		# load file
+		try:
+			fh = open(abspath)
+		except:
+			die("Couldn't load \"" + abspath + "\"")
+		self.importedfiles.append(abspath)
+		lines=fh.readlines()
+		fh.close()
+		# 
+
+		# concat all lines
+		alllines=""
+		for line in lines:
+			if line.find("#") != -1:
+				line = line[0:line.find("#")]
+			alllines += normalizeString(line)
+		# check for imports
+		while "@import(" in alllines:
+			start = alllines.find("@import(\"")
+			for i in range(start+9, len(alllines)):
+				if alllines[i] == "\"" and alllines[i-1] != "\\":
+					end=i
+					break
+			newfile = alllines[start+9:end]
+			for rule in self.__getRulesByFile(newfile, os.path.dirname(abspath)):
+				rules.append(rule)
+			alllines = alllines[:start] + alllines[end+3:]
+		# check for rules
+		while "." in alllines:
+			x = alllines[:alllines.find(".")+1]
+			if containsSubSigns(x):
+				self.subrules.append(SubRule(x))
+			else:
+				self.relrules.append(RelRule(x))
+			alllines = alllines[alllines.find(".")+1:]
+		return rules
+
+	def toString(self):
+		return str([x.toString() for x in self.subrules]) + " : " + str([x.toString() for x in self.relrules])
 
 class Substitution:
 	def __init__(self, subject, replacement, rule):
@@ -89,7 +152,14 @@ class Cell:
 			self.args.append(Cell(argstr))
 
 	def toString(self):
-		return normalizeString("TODO")
+		if len(self.args) == 0:
+			return normalizeString(self.body)
+		else:
+			tmp = self.body + "("
+			for arg in self.args:
+				tmp += arg.toString() + ","
+			tmp = tmp.strip(",") + ")"
+			return normalizeString(tmp)
 
 class SubRule:
 	def __init__(self, arg):
@@ -98,7 +168,7 @@ class SubRule:
 	def set(self, arg):
 		if not isinstance(arg, str):
 			die("SubRule::set() arg is not a string")
-		args = arg.split(getSubSigns())
+		args = splitAtSubSigns(arg)
 		self.cellA = Cell(args[0])
 		self.cellB = Cell(args[1])
 		self.sign = arg[len(args[0]):len(arg)-len(args[1])] # unsure
@@ -106,10 +176,8 @@ class SubRule:
 	def allowsSubstitution(self, sub):
 		return False
 
-	def doesStringContainSubSign(string):
-		if not isinstance(string, str):
-			die("SubRule::doesStringContainSubSign() string is not a string")
-		return getSubSigns() in string
+	def toString(self):
+		return self.cellA.toString() + self.sign + self.cellB.toString() + "."
 
 class RelRule:
 	def __init__(self, arg):
@@ -122,3 +190,5 @@ class RelRule:
 		if not isinstance(arg, str):
 			die("RelRule::set() arg is not a string")
 		self.cell = Cell(arg)
+	def toString(self):
+		return self.cell.toString() + "."
