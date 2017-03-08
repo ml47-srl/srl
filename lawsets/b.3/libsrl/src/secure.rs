2fe08d6b184195545e4512b4f6fd0a92ebf9d419
memoryleak47 <memoryleak47@protonmail.ch>
2017-03-08 17:53:22 +0100

    improved testing; +SecureCell::by_string

--- lawsets/b.3/libsrl/src/secure.rs
+++ lawsets/b.3/libsrl/src/secure.rs
@@ -1,11 +1,40 @@
 use cell::Cell;
+use parse::*;
+use parse::assemble::*;
+use parse::tokenize::*;
+use error::SRLError;
 
-pub struct SecureCell {
-	cell : Cell
-}
+pub struct SecureCell(Cell);
 
 impl SecureCell {
 	pub fn get_cell(&self) -> Cell {
-		self.cell.clone()
+		self.0.clone()
+	}
+
+	pub fn by_string(string : &str) -> Result<SecureCell, SRLError> {
+		if string.contains('.') {
+			return Err(SRLError("SecureCell::by_string".to_string(), "string contains '.'".to_string()));
+		}
+		match find_invalid_char(&string) {
+			Some(x) => return Err(SRLError("SecureCell::by_string".to_string(), format!("invalid char '{}'", string.chars().nth(x as usize).unwrap()))),
+			None => {}
+		}
+		let string : String = fix_whitespaces(string);
+		let tokens = match tokenize(string) {
+			Ok(x) => x,
+			Err(srl_error) => return Err(srl_error)
+		};
+		if ! check_paren_correctness(tokens.clone()) {
+			return Err(SRLError("Database::by_string()".to_string(), "Parens are not correct".to_string()));
+		}
+		let cell = match assemble(tokens) {
+			Ok(x) => x,
+			Err(srl_error) => return Err(srl_error)
+		};
+		let normalized = match cell.get_normalized() {
+			Ok(x) => x,
+			Err(srl_error) => return Err(srl_error)
+		};
+		Ok(SecureCell(normalized))
 	}
 }
--- lawsets/b.3/libsrl/tests/full.rs
+++ lawsets/b.3/libsrl/tests/full.rs
@@ -1,21 +1,35 @@
 extern crate libsrl;
 use libsrl::db::Database;
 use libsrl::navi::CellID;
+use libsrl::secure::SecureCell;
 
 #[test]
 fn test_full() {
-	let mut db = match Database::by_string("(= x y). {0 (p x)}.") {
+	let mut db = match Database::by_string("(= x y). {0 (p x)}. {0 (= (this 0) 0)}.") {
 		Ok(x) => x,
 		Err(_) => panic!("panic!")
 	};
 
 	assert_eq!(db.get_rule(1).to_rule_string(), "= x y.");
 	assert_eq!(db.get_rule(2).to_rule_string(), "{0 (p x)}.");
+	assert_eq!(db.get_rule(3).to_rule_string(), "{0 (= (this 0) 0)}.");
 	let evi = CellID::create(1, vec![]);
 	let src = CellID::create(2, vec![0, 1]);
 	match db.equals_law(src, evi) {
 		Ok(x) => { assert_eq!(x.to_rule_string(), "{0 (p y)}."); }
 		Err(_) => panic!("failure!")
 	};
-	assert_eq!(db.get_rule(3).to_rule_string(), "{0 (p y)}.");
+	assert_eq!(db.get_rule(4).to_rule_string(), "{0 (p y)}.");
+
+
+	let secure = match SecureCell::by_string("(a b)") {
+		Ok(x) => x,
+		Err(_) => panic!("error!")
+	};
+
+	let blub = CellID::create(3, vec![]);
+	match db.scope_insertion(blub, secure) {
+		Ok(x) => println!("rule: {}", x.to_rule_string()),
+		Err(srl_error) => panic!("panic: {:?}", srl_error)
+	}
 }
